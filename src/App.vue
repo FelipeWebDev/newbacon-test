88279d0fc335c1c8213b3a399d5486e0446b8188
Felipe Rodrigues <fehliveon@live.com>
2024-04-22 15:30:13 -0300

    feat: user listing by api

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <div id="app">
-    <header>
-
-    </header>
+    <UserListing />
   </div>
 </template>
 
+<script>
+import UserListing from "./components/UserListing.vue";
+
+const components = {
+  UserListing,
+};
+
+export default {
+  components,
+};
+</script>
+
 <style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 10px 50px;
+  
+  @media (min-width: 1068px) {
+    padding: 96px 348px;
+  }
+}
 </style>
